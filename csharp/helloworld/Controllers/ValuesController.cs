8a8ea4d66e8a7498d3c71663a475b3cd7d10e19d
Don Schenck <don.schenck@gmail.com>
2019-02-28 16:43:42 -0500

    Added Dockerfile for Node.js and csharp

---
+++ csharp/helloworld/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<String> Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
