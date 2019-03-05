a3f15e3b4ee4ba338da68be2032fb071a3cafcc1
Don Schenck <don.schenck@gmail.com>
2019-03-05 09:57:01 -0500

    More random updates

--- src/nodejs/helloworld/app.js
+++ src/nodejs/helloworld/app.js
@@ -3,6 +3,6 @@ const app = express()
 app.get('/', function(request,result){
     result.send('Hello World!')
 })
-app.listen(3333,function(){
+app.listen(3333,"0.0.0.0",function(){
     console.log('hello world app is listening on port 3333')
 })
